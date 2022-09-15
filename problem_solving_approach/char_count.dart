4cfc036cbd8f47864b704c900e0f2dfa1d9a73e8
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2022-09-15 15:25:13 +0200

    use function instead of regex

--- problem_solving_approach/char_count.dart
+++ problem_solving_approach/char_count.dart
@@ -2,11 +2,21 @@ Map<String, int> charCount(String str) {
   final result = <String, int>{};
   for (int i = 0; i < str.length; i++) {
     final char = str[i].toLowerCase();
-    if (RegExp(r'[a-z0-9]').hasMatch(char)) {
+    if (isAlphNumeric(char)) {
       result[char] = (result.containsKey(char)) ? result[char]! + 1 : 1;
     }
   }
   return result;
+}
+
+bool isAlphNumeric(String char) {
+  final code = char.codeUnitAt(0);
+  if (!(code > 47 && code < 58) &&
+      !(code > 64 && code < 91) &&
+      !(code > 96 && code < 123)) {
+        return false;
+      }
+  return true;
 }
 
 void main() {
