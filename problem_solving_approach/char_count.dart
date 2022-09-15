2539d8618cbe92decda0e3bcfd3af5a3af8b35e5
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2022-09-15 14:05:06 +0200

    lowercase char

--- problem_solving_approach/char_count.dart
+++ problem_solving_approach/char_count.dart
@@ -1,7 +1,7 @@
 Map<String, int> charCount(String str) {
   final result = <String, int>{};
   for (int i = 0; i < str.length; i++) {
-    final char = str[i];
+    final char = str[i].toLowerCase();
     if (result.containsKey(char)) {
       result[char] = result[char]! + 1;
     } else {
