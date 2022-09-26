a8c510e8462be6b626407f4c0b305d7db5741154
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2022-09-26 15:50:27 +0200

    max subarray sum using nested loops

---
+++ problem_solving_patterns/max_sublist_sum.dart
@@ -0,0 +1,25 @@
+import 'dart:developer';
+
+int? maxSublistSum(List<int> list, int num) {
+  if (num > list.length) {
+    return null;
+  }
+
+  int max = 0;
+
+  for (int i = 0; i < list.length - num + 1; i++) {
+    int temp = 0;
+    for (int j = 0; j < num ; j++) {
+      temp +=  list[i + j];
+    }
+    if (temp > max) {
+      max = temp;
+    }
+  }
+
+  return max;
+}
+
+void main() {
+  print(maxSublistSum([3,10,2,5,6],3));
+}
