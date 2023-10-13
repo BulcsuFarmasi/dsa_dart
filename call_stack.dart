cec5015986ac38d7980129d4032b65acc181d423
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2023-10-13 16:36:13 +0200

    add callstack

---
+++ call_stack.dart
@@ -0,0 +1,21 @@
+import 'dart:math';
+
+String takeShower() {
+  return 'Showering';
+}
+
+String eatBreakfast() {
+  final meal = cookMeal();
+  return 'Eating $meal';
+}
+
+String cookMeal() {
+  final List<String> items = ["Oatmeal", "Eggs", "Protein Shake"];
+  return items[Random().nextInt(items.length)];
+}
+
+void wakeUp() {
+  takeShower();
+  eatBreakfast();
+  print('Ok ready to go to work!');
+}
