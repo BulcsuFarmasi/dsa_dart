8eeac7cf8ec2d4a9db213e12906ca5326488ad31
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2024-04-02 10:23:02 +0200

    finish some recursive

--- recursion_challenging_problems/some_recursive.dart
+++ recursion_challenging_problems/some_recursive.dart
@@ -1,6 +1,18 @@
 bool isOdd(int value) => value % 2 != 0;
 
-bool someRecursive(List<int> list, dynamic) {
-  return false;
+bool someRecursive(List<int> list, dynamic callback) {
+  if (list.isEmpty) {
+    return false;
+  }
+
+  if (callback(list[0])) {
+    return true;
+  }
+
+  return someRecursive(list.sublist(1), callback);
+}
+
+void main() {
+  print(someRecursive([2,4], isOdd));
 }
 
